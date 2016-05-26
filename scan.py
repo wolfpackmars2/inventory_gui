00565fb64058f755471c59f4cc6059c7c7052f75
Steve Groesz <wolfpackmars2@yahoo.com>
2016-05-26 04:58:43 -0500

    initial commit

---
+++ scan.py
@@ -0,0 +1,154 @@
+import sys, cv2, datetime, base64, time
+# import os
+from PyQt4 import QtCore, QtGui
+from scanwindow import Ui_Form
+
+# TODO: Split files to maximum 250mb or less
+# TODO: Add camera selection to combobox
+# TODO: Add delay between camera.read operations
+# TODO: Test FRAMERATE of camera, round up, and use this to set camera read delay
+# TODO: for camera delay, use a timer ?
+# TODO: Add hot keys for buttons
+# TODO: Change format for records
+# TODO: Query UPC DB online for unique bar codes
+# TODO: Add tests!
+
+class CameraThread(QtCore.QThread):
+    pngSaved = QtCore.pyqtSignal(str)
+    camReady = QtCore.pyqtSignal()
+
+    def __init__(self, camera_port=0):
+        QtCore.QThread.__init__(self)
+        self.camera_port = camera_port
+        self.camera = cv2.VideoCapture(camera_port)
+        self.camera.set(3, 1600) # width
+        self.camera.set(4, 1200) # height
+        self.height = self.camera.get(4)
+        self.width = self.camera.get(3)
+        self.fps = self.getfps()
+        self.last_image = None
+
+
+    def __del__(self):
+        self.wait()
+
+    def run(self):
+        while(True):
+            retval, self.last_image = self.camera.read()
+            self.camReady.emit()
+
+    def getfps(self):
+        num_frames = 15
+        start = time.time()
+        for i in range(0, num_frames):
+            self.camera.read()
+        end = time.time()
+        seconds = end - start
+        fps = num_frames / seconds
+        return fps
+
+    def save(self, image_file):
+        """Save image to disk"""
+        cv2.imwrite(image_file, self.last_image)
+        self.pngSaved.emit(image_file)
+
+    def read(self):
+        return self.camera.read() # Returnvalue, PILImage
+
+class StartScan(QtGui.QWidget):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self.ui.btnTakePhoto.clicked.connect(self.snapshot)
+        self.ui.btnWrite.clicked.connect(self.writeout)
+        self.ui.btnRefreshCameras.clicked.connect(self.refreshCameras)
+        self.ui.cmbCamera.currentIndexChanged.connect(self.changeCamera)
+        self.ui.txtInput.returnPressed.connect(self.getinput)
+        self.camera_port = 0
+        self.camera = CameraThread(self.camera_port)
+        self.camera.camReady.connect(self.updateLiveView)
+        self.camera.pngSaved.connect(self.updatePreview)
+        self.camera.start()
+        self.last_text = ""
+        self.image_file = "./img"
+        self.saved_image_format = ".jpg"
+        self.live_image_format = ".jpg"
+        self.data_file = "./" + datetime.datetime.now().strftime('%Y%m%d.%H%M%S') + '.csv'
+        self.record_count = 0
+        fw = open(self.data_file, "w")
+        fw.write("ID,TimeStamp,UPC,PNGImage(Base64)\n")
+        fw.close()
+        #self.refreshCameras()
+
+    def changeCamera(self):
+        pass
+
+    def snapshot(self):
+        """Gets a frame from the camera"""
+        self.camera.save(self.image_file + self.saved_image_format)
+        self.ui.txtInput.setFocus()
+
+    def updateLiveView(self):
+        """Updates the live camera view"""
+        retval, img = cv2.imencode(self.live_image_format, self.camera.last_image)
+        im = QtGui.QImage.fromData(img)
+        pix = QtGui.QPixmap(im)
+        self.ui.lblLiveView.setPixmap(pix)
+
+    def updatePreview(self, image_file):
+        """Updates the preview window of the last image written"""
+        pix = QtGui.QPixmap(image_file)
+        pix = pix.scaled(self.ui.lblPreview.size(), QtCore.Qt.KeepAspectRatio)
+        self.ui.lblPreview.setPixmap(pix)
+
+    def refreshCameras(self):
+        if self.camera.isRunning():
+            self.camera.exit()
+        for i in range(10, 0, -1):
+            self.camera.camera = cv2.VideoCapture(i)
+            if self.camera.camera.isOpened():
+                self.ui.cmbCamera.clear()
+                for y in range(i, -1, -1):
+                    self.ui.cmbCamera.addItem(str(y), y)
+        if self.camera.isRunning() == False:
+            self.camera.start()
+
+
+
+    def writeout(self):
+        """Writes a record to the data file"""
+        if self.last_text != "":
+            f = open(self.image_file + self.saved_image_format, "rb").read()
+            fw = open(self.data_file, "a")
+            fw.write(str(self.record_count) +
+                     ',' +
+                     str(datetime.datetime.now()) +
+                     ',' +
+                     self.last_text +
+                     ',' +
+                     base64.b64encode(f).decode() +
+                     '\n')
+            fw.close()
+            self.last_text = ""
+            self.record_count += 1
+            self.ui.txtInput.setFocus()
+
+    def getinput(self):
+        """Takes the input from the input box"""
+        self.writeout()
+        if self.ui.txtInput.text() != "":
+            self.snapshot()
+            self.last_text = self.ui.txtInput.text()
+            self.ui.txtHistory.setPlainText(str(self.record_count) + ": " + self.last_text + "\n" + self.ui.txtHistory.toPlainText())
+            self.ui.txtInput.setText("")
+        else:
+            self.close()
+
+
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    myapp = StartScan()
+    myapp.show()
+    sys.exit(app.exec_())
